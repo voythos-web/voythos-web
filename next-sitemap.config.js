2b3304569c8a4fd70b57188d2e9f960d5ee392c8
Sadie Lee <sadiemlee25@gmail.com>
2025-07-01 15:53:23 -0700

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.voythos.io',
+  generateRobotsTxt: true, // generates /robots.txt
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+};
