bf9276050447d51bc7aff4d5dda06984fbd5b1d2
Vasu Anjur <vasuanjurthupil@gmail.com>
2025-10-20 16:37:10 -0500

    test commit, removed a comma

--- app/news/May-3-25/May3Client.tsx
+++ app/news/May-3-25/May3Client.tsx
@@ -8,7 +8,7 @@ const May3Client = () => {
             <NewsPage 
                 heading="Voythos featured in Mayo Clinic Platform_ Accelerate showcase"
                 media="/images/news/accelerate_news.svg"
-                bodytext="Voythos is excited to be part of the current Mayo Clinic Platform_Accelerate cohort, recently featured in a program showcase highlighting the companies driving new advances in health technology. As part of the program, we are focused on advancing our multimodal AI platform for the prediction of aortic disease, with the goal of improving patient outcomes through earlier and more accurate intervention."
+                bodytext="Voythos is excited to be part of the current Mayo Clinic Platform_Accelerate cohort recently featured in a program showcase highlighting the companies driving new advances in health technology. As part of the program, we are focused on advancing our multimodal AI platform for the prediction of aortic disease, with the goal of improving patient outcomes through earlier and more accurate intervention."
                 href="https://newsnetwork.mayoclinic.org/discussion/mayo-clinic-platform_accelerate-showcase-celebrates-promise-of-health-tech-innovation-for-improving-patient-care/"
                 nextpage="/news/Apr-18-25"
                 date="May 3, 2025"
